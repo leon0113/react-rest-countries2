95ed4915dc0938dbd7f31aa215cac50291b6f161
leon0113 <tahjib.leon399@gmail.com>
2023-01-13 17:00:44 +0600

    creating components

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country'
 
 const Countries = () => {
+    const [countries,setCountries] = useState([]);
+   
+    useEffect(()=>{
+       
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+
+    },[])
     return (
         <div>
-            <h2>Countries in other file</h2>
+           <h1>Hello from Countries</h1> 
+           <p>{countries.length}</p>
+           {
+            countries.map( country => <Country></Country>)
+           }
         </div>
     );
 };
