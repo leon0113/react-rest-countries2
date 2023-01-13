472374dcd189890cac90e8688e46caf20d16e9b2
leon0113 <tahjib.leon399@gmail.com>
2023-01-13 17:15:08 +0600

    data showing

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -16,7 +16,7 @@ const Countries = () => {
            <h1>Hello from Countries</h1> 
            <p>{countries.length}</p>
            {
-            countries.map( country => <Country></Country>)
+            countries.map( country => <Country name={country.name.common} population={country.population} img={country.flags.png}></Country>)
            }
         </div>
     );
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import './Country.css'
 const Country = (props) => {
     return (
-        <div>
+        <div className='country'>
             <h2>Country name : {props.name}</h2>
+            <p>Population : {props.population}</p>
+            <img src={props.img} alt="" />
         </div>
     );
 };
