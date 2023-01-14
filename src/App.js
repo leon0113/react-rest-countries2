136f9b3b30f451aeb9726c41397429ed2c6de770
leon0113 <tahjib.leon399@gmail.com>
2023-01-14 20:22:57 +0600

    operation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 
@@ -14,15 +15,26 @@ function App() {
 const districtStyle={
   border : '2px solid black',
   borderRadius : '100px', 
+  backgroundColor : 'lightyellow',
+  margin : '20px',
+  padding : '20px'
+}
+const districtStyle1={
+  border : '2px solid black',
+  borderRadius : '100px', 
   backgroundColor : 'yellow',
   margin : '20px',
   padding : '20px'
 }
 function District(props){
+  const [power,setPower] = useState(1)
+  const increasePower = () => setPower(power * 5);
   return (
     <div style={districtStyle}>
       <h2>Name: {props.name}</h2>
       <p>Speciality: {props.speciality}</p>
+      <h4>Power: {power} </h4>
+      <button style={districtStyle1} onClick={increasePower}>Boost</button>
     </div>
   )
 }
